70d12d47b750700096cb0ecdfcc032cc73c133a9
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2025-08-18 14:21:40 -0400

    Creando Flujo

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(requests):
+    contexto = {"Nombre": "Panel de Dispositivos EcoEnergy"}
+    productos = [{"nombre" : "Sensor de la puerta", "valor": 20},
+                 {"nombre": "Sensor de la ventana", "valor": 120},
+                 {"nombre": "Sensor del piso", "valor": 10},
+                 {"nombre": "Sensor del Ventilador", "valor": 100}]
+    
+    consumo_max = 100
+
+    return render(requests, "dispositivos/panel.html",{
+        "contexto": contexto,
+        "productos": productos,
+        "Consumo_Maximo": consumo_max
+
+    })
