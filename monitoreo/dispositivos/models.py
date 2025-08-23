67cc6eaa178f104c8968806f9babe811ac740f9c
Francisco Astudillo <fco.exe@gmail.com>
2025-08-23 10:24:52 -0400

    Creando Tablas Categoría, Zona y Dispositivo

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -2,17 +2,27 @@ from django.db import models
 
 # Create your models here.
 class Categoria(models.Model):
+
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return self.nombre
 
+class Zona(models.Model):
+    nombre = models.CharField(max_length=150)
+    ubicacion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
     consumo_Maximo = models.IntegerField()
     estado = models.BooleanField(default=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE,null=True)
+    zona = models.ForeignKey(Zona,on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.nombre
