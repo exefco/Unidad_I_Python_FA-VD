c367cf9ded60085eb68e78d68e224c7ae1e74b00
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2025-08-25 13:22:58 -0400

    Añadimos la clase BaseModel a models.py

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 
-# Create your models here.
-class Categoria(models.Model):
+class BaseModel(model.Model):
+    ESTADOS = [
+        ("ACTIVO","Activo"."INACTIVO","Inactivo")
+    ]
+    estado = models.CharField(max_length=10,choises = ESTADOS, default = "ACTIVO")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+class Categoria(BaseModel):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return self.nombre
 
-class Zona(models.Model):
+class Zona(BaseModel):
     nombre = models.CharField(max_length=150)
     ubicacion = models.CharField(max_length=250)
 
@@ -16,7 +27,7 @@ class Zona(models.Model):
         return self.nombre
 
 
-class Dispositivo(models.Model):
+class Dispositivo(BaseModel):
     nombre = models.CharField(max_length=100)
     consumo_Maximo = models.IntegerField()
     estado = models.BooleanField(default=True)
@@ -26,7 +37,7 @@ class Dispositivo(models.Model):
     def __str__(self):
         return self.nombre
     
-class Medicion(models.Model):
+class Medicion(BaseModel):
     fecha = models.DateTimeField(auto_now_add=True)
     consumo_registrado = models.IntegerField()
     dispositivo = models.ForeignKey(Dispositivo,on_delete=models.CASCADE,null=True)
@@ -34,7 +45,7 @@ class Medicion(models.Model):
     def __str__(self):
         return f"Medición con fecha: {self.fecha} del dispositivo: {self.dispositivo.nombre}, consumo: {self.consumo_registrado}"
     
-class Alerta(models.Model):
+class Alerta(BaseModel):
     mensaje = models.CharField(max_length=250)
     nivel_alerta = models.CharField(max_length=50)
     fecha = models.DateTimeField(auto_now_add=True)
